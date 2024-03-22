b6294aab247aa88bd2219463ac6c3af8ebd0c4dd
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2024-03-22 15:22:29 +0100

    add PC config

--- utils.py
+++ utils.py
@@ -7,8 +7,10 @@ def read_config():
     print(computer_name)
     if computer_name == "DESKTOP-SAI3PJD":
         config_path = 'H:\\Documents\\1_dokt\\other_programs\\eeggan\\settings_laptop.ini'
+    elif computer_name == 'DESKTOP-CM8AHTB':
+        config_path = 'C:\\Users\przem\\OneDrive - Politechnika Warszawska\Dokumenty\\1_dokt\\other_programs_PC\\eeggan\\settings_PC.ini'
     else:
-        raise Exception("Computer name not found")
+        raise Exception(f"Computer name not found {computer_name}")
 
     config = configparser.ConfigParser()
     config.read(config_path)
